a0f5c811b7ca6b55db2794d4cbb63080611bf4da
Sunbekova <123359531+Sunbekova@users.noreply.github.com>
2023-06-28 15:14:21 +0600

    Update snake.py

--- TSIS_10_11/snake.py
+++ TSIS_10_11/snake.py
@@ -10,7 +10,7 @@ conn = psycopg2.connect(
     host = "localhost"
     , dbname = 'postgres'
     , user = 'postgres'
-    , password = '151207'
+    , password = ''
     , port = 5432
 )
 cur = conn.cursor()
